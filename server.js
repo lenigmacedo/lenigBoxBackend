6db78e0e6a3c7a8035a49a514d391cdcf7b70d0b
Lennyk <43722810+lenigmacedo@users.noreply.github.com>
2020-02-12 13:42:29 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const cors = require("cors");
+
+app.use(cors());
+
+io.on("connection", socket => {
+  socket.on("connectRoom", box => {
+    socket.join(box);
+  });
+});
+
+mongoose.connect(
+  "mongodb+srv://Lennyk:simone20@lenigbox-h5zen.gcp.mongodb.net/lenigBox?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true
+  }
+);
+
+app.use((req, res, next) => {
+  req.io = io;
+
+  return next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/files", express.static(path.resolve(__dirname, "..", "tmp")));
+
+app.use(require("./routes"));
+
+server.listen(process.env.PORT || 3333);
